ed3a1e5949aabc03388dec0fdcd7c2719658b376
Mark  King <g34519838@gmail.com>
2024-09-30 11:00:23 -0700

    End of days commit

--- process.py
+++ process.py
@@ -7,13 +7,16 @@ lorem_dict = {}
 print(lorem_dict)
 
 
-for line in file:
-    for word in line:
-        print(word)
-        if word in lorem_dict:
-            lorem_dict[word]+=1
-        else:
-            lorem_dict[word] = 1
+
+for word in file.read():
+    print(word)
+    # for word in line:
+    #     # print(word)
+    #     if word in lorem_dict:
+    #         lorem_dict[word]+=1
+    #     else:
+    #         lorem_dict[word] = 1
 
 print(lorem_dict)
+print(type(file))
 
