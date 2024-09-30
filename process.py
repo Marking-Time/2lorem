a6c88fc2f07580eff0c673a59e0d9d885ca4b177
Mark  King <g34519838@gmail.com>
2024-09-30 10:31:34 -0700

    Begin creation of lorem freq

--- process.py
+++ process.py
@@ -1,2 +1,19 @@
-with open("../data/lorem.txt") as file:
-    print(file.read())
+file =  open("../data/lorem.txt")
+
+
+# print(file.readline())
+
+lorem_dict = {}
+print(lorem_dict)
+
+
+for line in file:
+    for word in line:
+        print(word)
+        if word in lorem_dict:
+            lorem_dict[word]+=1
+        else:
+            lorem_dict[word] = 1
+
+print(lorem_dict)
+
