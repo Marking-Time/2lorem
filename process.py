ebacd14aed0f923099029d5108c097772c306111
Mark  King <g34519838@gmail.com>
2024-10-01 09:51:48 -0700

    Remove unwanted characters in process:lorem_dict

--- process.py
+++ process.py
@@ -9,18 +9,28 @@ print(lorem_dict)
 words = open("../data/lorem.txt").read().split()
 
 for word in words:
-    # print(word)
+    if word.endswith('.'):
+        word = word[:-1]
+    elif word.endswith(','):
+        word = word[:-1]
+    elif word.endswith('?'):
+        word = word[:-1] 
+    elif word.endswith('!'):
+        word = word[:-1]
+    print(word)
+
     if word in lorem_dict:
         lorem_dict[word]+=1
     else:
         lorem_dict[word] = 1
     # for word in line:
-    #     # print(word)
-    #     if word in lorem_dict:
-    #         lorem_dict[word]+=1
-    #     else:
-    #         lorem_dict[word] = 1
+        # # print(word)
+        # if word in lorem_dict:
+        #     lorem_dict[word]+=1
+        # else:
+        #     lorem_dict[word] = 1
 
 print(lorem_dict)
+print(len(lorem_dict))
 # print(type(file))
 
