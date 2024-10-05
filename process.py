6e023978293bf3ef8a290340178f0665b1989ec5
Mark  King <g34519838@gmail.com>
2024-10-05 09:10:23 -0700

    End of day - Conpleted eng words to a list

--- process.py
+++ process.py
@@ -1,6 +1,23 @@
 import re
 
-file =  open("../data/en-freq-10k.txt")
+# eng_fin = []
+
+fin_list = []
+
+with open("../data/en-freq-10k.txt", "r") as file:
+    for contents in file:
+        eng_fin = []
+        contents = file.readline()
+        print(contents)
+        y_position = contents.find(",")
+        print(y_position)
+        eng_fin.append(contents[ :y_position])
+        print(eng_fin)
+        fin_list.append(eng_fin)
+
+# print(eng_fin)
+
+# file =  open("../data/en-freq-10k.txt").readline()
 
 # print(type(file))
 
@@ -92,34 +109,49 @@ print(type(file))
 # print(eng_word)
 
 
-eng_words = []
-print(file)
-for line in file:
-    # print(file.readline())
-    x = file.readline()
-    eng_words.append(x)
+# eng_words = []
+# print(file)
+# for line in file:
+#     # print(file.readline())
+#     x = file.readline()
+#     eng_words.append(x)
 
 
 print("=================================== print eng_Words")
 
 exp = "([A-Za-z])\w+"
 
-print(eng_words)
+# print(eng_words)
 
 eng_fin = []
 
-for string in eng_words:
-    if type(string) == None:
-        print()
-    else:   
-        x = re.search(r"([A-Za-z])\w+",string)
-        x = str(x)
-        # eng_fin.append(x)
-        x = x.split()
-        print(x)
+# for string in eng_words:
+#     if type(string) == None:
+#         print()
+#     else:   
+#         x = re.search(r"([A-Za-z])\w+",string)
+#         x = str(x)
+#         # eng_fin.append(x)
+#         x = x.split()
+#         print(x)
 
 
-print(eng_fin)
+# print(eng_fin)
 
 # <----------file type text wrapper object------------>
 print(type(file))
+
+print(contents)
+
+print("type contents: "+str(type(contents))) 
+
+
+
+
+
+# for line in file:
+    # print(line)
+
+print(eng_fin)
+
+print(fin_list)
