3f49c48e37bd77de929a6b9f6eee8e7777fc2588
Mark  King <g34519838@gmail.com>
2024-10-11 08:51:44 -0700

    working on jason import export

--- tolorem.py
+++ tolorem.py
@@ -3,7 +3,15 @@
 
 input = "1WASHINGTON 23 Sen.999 Dianne994321 Feinstein, D-Calif., a vocal advocate of gun control measures who was known for trying to find common  ground with Republicans during her three decades in the Senate, has died, her office confirmed on Friday She was  "
 
+with open("eng2Lorem_dict.txt","r") as eng2:
+    print(eng2)
+    print(eng2.readlines())
+
+
+# print(input)
+
 # english_word = input.split()
 # # print(english_word)
 
 # f = open("english_words.txt","x")
+
