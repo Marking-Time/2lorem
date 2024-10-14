0a93ae8b46b143906e0cef69edf8ab813df6d2f1
Mark  King <g34519838@gmail.com>
2024-10-14 13:43:30 -0700

    Got json file into puthon dict

--- tolorem.py
+++ tolorem.py
@@ -1,11 +1,13 @@
 # file = open("lorem.txt")
 # print(file.read())
+import json
 
 input = "1WASHINGTON 23 Sen.999 Dianne994321 Feinstein, D-Calif., a vocal advocate of gun control measures who was known for trying to find common  ground with Republicans during her three decades in the Senate, has died, her office confirmed on Friday She was  "
 
 with open("eng_2_lorem.json","r") as eng2:
-    print(eng2)
-    print(eng2.readlines())
+    eng2lorem = json.load(eng2)
+    print(eng2lorem)
+    # print(eng2.readlines())
 
 
 # print(input)
