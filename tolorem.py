645f57822108fa325e26124ecb95199552a54c0d
Mark  King <g34519838@gmail.com>
2024-10-14 13:38:43 -0700

    open new jason file

--- tolorem.py
+++ tolorem.py
@@ -3,7 +3,7 @@
 
 input = "1WASHINGTON 23 Sen.999 Dianne994321 Feinstein, D-Calif., a vocal advocate of gun control measures who was known for trying to find common  ground with Republicans during her three decades in the Senate, has died, her office confirmed on Friday She was  "
 
-with open("eng2Lorem_dict.txt","r") as eng2:
+with open("eng_2_lorem.json","r") as eng2:
     print(eng2)
     print(eng2.readlines())
 
