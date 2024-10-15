65bf2cbcb5fcd5455165a06fb8092f691788ed1a
Mark  King <g34519838@gmail.com>
2024-10-15 08:49:22 -0700

    work on input

--- tolorem.py
+++ tolorem.py
@@ -6,12 +6,14 @@ input = "1WASHINGTON 23 Sen.999 Dianne994321 Feinstein, D-Calif., a vocal advoca
 
 with open("eng_2_lorem.json","r") as eng2:
     eng2lorem = json.load(eng2)
-    print(eng2lorem)
-    print(type(eng2lorem))
+    # print(eng2lorem)
+   
     # print(eng2.readlines())
 
+print(eng2lorem)
+print(type(eng2lorem))
 
-# print(input)
+print(input)
 
 # english_word = input.split()
 # # print(english_word)
