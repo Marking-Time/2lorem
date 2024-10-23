444be6952bb621ee2d32ebbfb42fdee7e39a7462
Mark  King <g34519838@gmail.com>
2024-10-22 21:06:37 -0700

    create toMorse.py to convert eng to morse code - need to handle charaters not in morse

---
+++ toMorse.py
@@ -0,0 +1,17 @@
+import json
+
+eng  = "1WASHINGTON 23 Sen.999 Dianne994321 Feinstein, D-Calif., a vocal advocate of gun control measures who was known for trying to find common  ground with Republicans during her three decades in the Senate, has died, her office confirmed on Friday She was "
+morse = ""
+
+eng = eng.lower()
+
+print(eng)
+
+
+with open("eng_2_norse.json","r") as eng2:
+    eng2 = json.load(eng2)
+    for word in eng:
+        for letter in word:
+            morse  = morse + eng2[letter]
+
+print(morse)
