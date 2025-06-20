f514c61e4b013366764183016def874ea71cef62
Mark  King <g34519838@gmail.com>
2025-06-20 13:47:30 -0700

    add input method to toMorese.py

--- toMorse.py
+++ toMorse.py
@@ -1,6 +1,7 @@
 import json
 
-eng  = "1WASHINGTON 23 Sen.999 Dianne994321 Feinstein, D-Calif., a vocal advocate of gun control measures who was known for trying to find common  ground with Republicans during her three decades in the Senate, has died, her office confirmed on Friday She was "
+# eng  = "1WASHINGTON 23 Sen.999 Dianne994321 Feinstein, D-Calif., a vocal advocate of gun control measures who was known for trying to find common  ground with Republicans during her three decades in the Senate, has died, her office confirmed on Friday She was "
+eng = input("please enter some text: \n")
 morse = ""
 
 eng = eng.lower()
