5bca29607d2e2d4ae06927c4b0208aa899811e40
Mark  King <g34519838@gmail.com>
2025-09-08 10:55:49 -0700

    clean up toMorse.py

--- toMorse.py
+++ toMorse.py
@@ -6,13 +6,12 @@ morse = ""
 
 eng = eng.lower()
 
-print(eng)
-
-
 with open("eng_2_norse.json","r") as eng2:
     eng2 = json.load(eng2)
     for word in eng:
+        word = word + " "
         for letter in word:
+            
             morse  = morse + eng2[letter]
 
 print(morse)
