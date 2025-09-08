aae30c9388a170cdf95d4ca624d1612370fabdbd
Mark  King <g34519838@gmail.com>
2025-09-08 16:48:42 -0700

    document toMorse

--- toMorse.py
+++ toMorse.py
@@ -1,17 +1,14 @@
 import json
-
-# eng  = "1WASHINGTON 23 Sen.999 Dianne994321 Feinstein, D-Calif., a vocal advocate of gun control measures who was known for trying to find common  ground with Republicans during her three decades in the Senate, has died, her office confirmed on Friday She was "
+# Code converts english text to morse code
 eng = input("please enter some text: \n")
 morse = ""
-
 eng = eng.lower()
 
 with open("eng_2_norse.json","r") as eng2:
     eng2 = json.load(eng2)
     for word in eng:
         word = word + " "
-        for letter in word:
-            
+        for letter in word:            
             morse  = morse + eng2[letter]
 
 print(morse)
